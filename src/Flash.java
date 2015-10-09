04cce0f5da59a92defc4160c759c2bf2a021e9a7
miguel <miguel.angel.lopez@correounivalle.edu.co>
2015-10-09 15:56:49 -0500

    Add main

---
+++ src/Flash.java
@@ -0,0 +1,15 @@
+/***********************************************
+ * Autor: Camilo Ruiz Casanova - 1324486
+ * Autor: Miguel Angel Lopez - 1326691
+ * Autor: Andres Felipe Polanco - 1324539
+ * Fecha: 09-oct-2015
+ * Nombre del Archivo: Flash.java
+ * Plan: Ingeniería de Sistemas - 3743
+ * Institución Educativa: Universidad del Valle
+ **********************************************/
+
+public class Flash {
+    public static void main(String[] args) {
+
+    }
+}
