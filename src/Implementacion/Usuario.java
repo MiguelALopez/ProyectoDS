ab28b991fb1947062e4ec1f2dda0168dc4e1acc8
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2015-10-13 21:42:58 -0500

    Clase Usuario

---
+++ src/Implementacion/Usuario.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Implementacion;
+
+/**
+ *
+ * @author Camilo Ruiz Casanova
+ */
+public class Usuario 
+{
+    private String cedula;
+    private String clave;
+    private String rol;
+    private String nombre;
+    private String direccion;
+    private String fechaNacimiento;
+    private String telefono;
+    private String celular;
+    private String fechaIncorporacion;
+    private double salario;
+    private String numeroCuenta;
+    private String estado;
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getFechaIncorporacion() {
+        return fechaIncorporacion;
+    }
+
+    public void setFechaIncorporacion(String fechaIncorporacion) {
+        this.fechaIncorporacion = fechaIncorporacion;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }    
+}
