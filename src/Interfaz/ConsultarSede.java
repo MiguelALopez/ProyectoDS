06eb0e0b57501d952b64bca5b465895a41bdd3d9
miguel <miguel.angel.lopez@correounivalle.edu.co>
2015-10-14 19:47:28 -0500

    Interfaz sedes terminada

---
+++ src/Interfaz/ConsultarSede.java
@@ -0,0 +1,168 @@
+/***********************************************
+ * Autor: Miguel Angel Lopez Fernandez
+ * Código: 1326691
+ * Fecha: 14-oct-2015
+ * Nombre del Archivo: ConsultarSede.java
+ * Plan: Ingeniería de Sistemas - 3743
+ * Institución Educativa: Universidad del Valle
+ **********************************************/
+package Interfaz;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ConsultarSede extends JFrame {
+
+    JTextField textNumSede;
+    JTextField textNameSede;
+    JTextField textAddress;
+    JTextField textManager;
+    JTextField textBudget;
+    JTextField textNumEmployee;
+    JTextField textNumTruck;
+    JButton buttonAccept;
+    JButton buttonCancel;
+
+    public ConsultarSede() {
+        super("Modificar Sede");
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Se inicializan los componentes
+        textNumSede = new JTextField();
+        textNameSede = new JTextField();
+        textAddress = new JTextField();
+        textManager = new JTextField();
+        textBudget = new JTextField();
+        textNumEmployee = new JTextField();
+        textNumTruck = new JTextField();
+        buttonAccept = new JButton("Aceptar");
+        buttonCancel = new JButton("Cancelar");
+
+        // Label titulo
+        manageConst(c, 0, 0, 2, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.CENTER;
+        add(new JLabel("Panel de Modificacion de Sedes"), c);
+
+        // Linea separadora
+        manageConst(c, 0, 1, 2, 1, 0, 0, 3, 3);
+        c.insets = new Insets(0, 30, 30, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(new JSeparator(SwingConstants.HORIZONTAL), c);
+
+
+        // Label Numero Sede
+        manageConst(c, 0, 2, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Numero Sede"), c);
+
+        manageConst(c, 1, 2, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textNumSede, c);
+
+        // Label Nombre Sede
+        manageConst(c, 0, 3, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Nombre de la Sede"), c);
+
+        manageConst(c, 1, 3, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textNameSede, c);
+
+        // Label Direccion
+        manageConst(c, 0, 4, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Direccion"), c);
+
+        manageConst(c, 1, 4, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textAddress, c);
+
+        // Label Gerente
+        manageConst(c, 0, 5, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Gerente a Cargo"), c);
+
+        manageConst(c, 1, 5, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textManager, c);
+
+        // Label Presupuesto
+        manageConst(c, 0, 6, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Presupuesto Asignado"), c);
+
+        manageConst(c, 1, 6, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textBudget, c);
+
+        // Label Presupuesto
+        manageConst(c, 0, 7, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Presupuesto Asignado"), c);
+
+        manageConst(c, 1, 7, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textNumEmployee, c);
+
+        // Label Camiones
+        manageConst(c, 0, 8, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 30, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Numero de Camiones"), c);
+
+        manageConst(c, 1, 8, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textNumTruck, c);
+
+        // Button Cancelar
+        manageConst(c, 0, 9, 1, 1, 0, 0, 0, 0);
+        c.insets = new Insets(15, 20, 0, 30);
+        c.anchor = GridBagConstraints.CENTER;
+        add(buttonCancel, c);
+
+        // Button Aceptar
+        manageConst(c, 1, 9, 1, 1, 0, 0, 0, 0);
+        c.insets = new Insets(15, 20, 0, 30);
+        c.anchor = GridBagConstraints.CENTER;
+        add(buttonAccept, c);
+
+
+        setVisible(true);
+        setSize(400, 450);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void manageConst(GridBagConstraints c, int colX, int filY, int width, int heigth, int weightx, int weighty, int ipadx, int ipady) {
+        c.gridx = colX;
+        c.gridy = filY;
+        c.gridheight = heigth;
+        c.gridwidth = width;
+        c.weightx = weightx;
+        c.weighty = weighty;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.ipadx = ipadx;
+        c.ipady = ipady;
+    }
+
+    public static void main(String[] args) {
+        ConsultarSede prueba = new ConsultarSede();
+
+    }
+}
--- src/Interfaz/CrearSede.java
+++ src/Interfaz/CrearSede.java
@@ -10,34 +10,15 @@ package Interfaz;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
-public class CrearSede extends JFrame{
+public class CrearSede extends JFrame {
 
     JTextField textNumSede;
     JTextField textNameSede;
     JTextField textAddress;
+    JButton buttonCrear;
 
-    public CrearSede(){
-        /*super("Crear Sede");
-        setLayout(new GridLayout(3, 2, 5, 5));
-
-        // Se inicializan los componentes
-        textNumSede = new JTextField();
-        textNameSede = new JTextField();
-        textAddress = new JTextField();
-
-        add(new JLabel("Numero Sede"));
-        add(textNumSede);
-        add(new JLabel("Nombre de la Sede"));
-        add(textNameSede);
-        add(new JLabel("Direccion"));
-        add(textAddress);
-
-
-        setVisible(true);
-        setSize(400, 200);*/
-
+    public CrearSede() {
         super("Crear Sede");
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -46,27 +27,46 @@ public class CrearSede extends JFrame{
         textNumSede = new JTextField();
         textNameSede = new JTextField();
         textAddress = new JTextField();
+        buttonCrear = new JButton("Crear");
 
-        manageConst(c,0,0,1,1,0,0);
-        add(new JLabel("Numero Sede"),c);
+        // Label Numero Sede
+        manageConst(c, 0, 0, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 15, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Numero Sede"), c);
 
-        manageConst(c,1,0,1,1,1,0);
+        manageConst(c, 1, 0, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
         c.fill = GridBagConstraints.HORIZONTAL;
-        add(textNumSede,c);
+        add(textNumSede, c);
 
-        manageConst(c,0,1,1,1,0,0);
-        add(new JLabel("Nombre de la Sede"),c);
+        // Label Nombre Sede
+        manageConst(c, 0, 1, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 15, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Nombre de la Sede"), c);
 
-        manageConst(c,1,1,1,1,1,0);
+        manageConst(c, 1, 1, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
         c.fill = GridBagConstraints.HORIZONTAL;
-        add(textNameSede,c);
+        add(textNameSede, c);
 
-        manageConst(c,0,2,1,1,0,0);
-        add(new JLabel("Direccion"),c);
+        // Label Direccion
+        manageConst(c, 0, 2, 1, 1, 0, 0, 3, 3);
+        c.insets = new Insets(10, 15, 10, 0);
+        c.anchor = GridBagConstraints.WEST;
+        add(new JLabel("Direccion"), c);
 
-        manageConst(c,1,2,1,1,1,0);
+        manageConst(c, 1, 2, 1, 1, 1, 0, 3, 3);
+        c.insets = new Insets(0, 20, 0, 30);
         c.fill = GridBagConstraints.HORIZONTAL;
-        add(textAddress,c);
+        add(textAddress, c);
+
+        // Button Crear
+        manageConst(c, 1, 3, 1, 1, 0, 0, 0, 0);
+        c.insets = new Insets(10, 20, 0, 30);
+        c.anchor = GridBagConstraints.EAST;
+        add(buttonCrear, c);
 
 
         setVisible(true);
@@ -74,20 +74,19 @@ public class CrearSede extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void manageConst(GridBagConstraints c, int colX, int filY, int width, int heigth, int weightx, int weighty){
+    public void manageConst(GridBagConstraints c, int colX, int filY, int width, int heigth, int weightx, int weighty, int ipadx, int ipady) {
         c.gridx = colX;
         c.gridy = filY;
         c.gridheight = heigth;
         c.gridwidth = width;
         c.weightx = weightx;
         c.weighty = weighty;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
-        c.insets = new Insets(10,10,10,10);
-//        c.ipadx = 50;
-//        c.ipady = 50;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.ipadx = ipadx;
+        c.ipady = ipady;
     }
-
 
 
     public static void main(String[] args) {
