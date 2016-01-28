227c24a0458dc75f1447521dabee60e6de45e4de
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2016-01-28 17:04:23 -0500

    cambios semilla

--- src/Semilla.sql
+++ src/Semilla.sql
@@ -12,11 +12,11 @@
 DROP TABLE IF EXISTS sede CASCADE;
 CREATE TABLE sede
 (
-    sede_numero        VARCHAR(5),
-    sede_nombre        VARCHAR(90)  NOT NULL,
-    sede_direccion     VARCHAR(90)  NOT NULL,
-    sede_ciudad        VARCHAR(90)  NOT NULL,
-    sede_gerente       VARCHAR(30),
+    sede_numero        VARCHAR  NOT NULL,
+    sede_nombre        VARCHAR  NOT NULL,
+    sede_direccion     VARCHAR  NOT NULL,
+    sede_ciudad        VARCHAR  NOT NULL,
+    sede_gerente       VARCHAR,
     sede_cant_camiones INTEGER,    
     PRIMARY KEY (sede_numero)
 );
@@ -24,19 +24,19 @@ CREATE TABLE sede
 DROP TABLE IF EXISTS usuario CASCADE;
 CREATE TABLE usuario
 (
-    usuario_cedula              VARCHAR(30),
-    usuario_passwd              VARCHAR(90) NOT NULL,
-    usuario_nombre              VARCHAR(90) NOT NULL,
-    usuario_apellido            VARCHAR(90) NOT NULL,
-    usuario_rol                 VARCHAR(60) NOT NULL,
-    usuario_estado              VARCHAR(30) NOT NULL,
-    usuario_fecha_nacimiento    VARCHAR(25),
-    usuario_direccion           VARCHAR(90),
-    usuario_telefono            VARCHAR(30),    
-    usuario_fecha_incorporacion VARCHAR(25),
-    usuario_salario             VARCHAR(30),
-    usuario_cuenta              VARCHAR(30),
-    usuario_sede_numero         VARCHAR(5),
+    usuario_cedula              VARCHAR NOT NULL,
+    usuario_passwd              VARCHAR NOT NULL,
+    usuario_nombre              VARCHAR NOT NULL,
+    usuario_apellido            VARCHAR NOT NULL,
+    usuario_rol                 VARCHAR NOT NULL,
+    usuario_estado              VARCHAR NOT NULL,
+    usuario_fecha_nacimiento    VARCHAR,
+    usuario_direccion           VARCHAR,
+    usuario_telefono            VARCHAR,    
+    usuario_fecha_incorporacion VARCHAR,
+    usuario_salario             VARCHAR,
+    usuario_cuenta              VARCHAR,
+    usuario_sede_numero         VARCHAR,
     PRIMARY KEY (usuario_cedula),
     FOREIGN KEY (usuario_sede_numero) REFERENCES sede (sede_numero)
 );
@@ -49,13 +49,13 @@ DROP TABLE IF EXISTS pqr CASCADE;
 CREATE TABLE pqr
 (
     pqr_numero      VARCHAR DEFAULT nextval('pqr_seq'::regclass) NOT NULL,
-    pqr_fecha       DATE        NOT NULL,  
-    pqr_tipo        VARCHAR(30) NOT NULL,
-    pqr_contenido   TEXT        NOT NULL,
-    pqr_estado      VARCHAR(30) NOT NULL,
-    pqr_cedula      VARCHAR(30) NOT NULL,
-    pqr_nombre      VARCHAR(90) NOT NULL,
-    pqr_sede        VARCHAR(5)  NOT NULL,
+    pqr_fecha       DATE    NOT NULL,  
+    pqr_tipo        VARCHAR NOT NULL,
+    pqr_contenido   TEXT    NOT NULL,
+    pqr_estado      VARCHAR NOT NULL,
+    pqr_cedula      VARCHAR NOT NULL,
+    pqr_nombre      VARCHAR NOT NULL,
+    pqr_sede        VARCHAR NOT NULL,
     PRIMARY KEY (pqr_numero),
     FOREIGN KEY (pqr_sede) REFERENCES sede (sede_numero)
 );
@@ -63,9 +63,9 @@ CREATE TABLE pqr
 DROP TABLE IF EXISTS pos CASCADE;
 CREATE TABLE pos
 (
-    pos_id        VARCHAR(5),
-    pos_nombre    VARCHAR(60) NOT NULL,
-    pos_direccion VARCHAR(90) NOT NULL,
+    pos_id        VARCHAR NOT NULL,
+    pos_nombre    VARCHAR NOT NULL,
+    pos_direccion VARCHAR NOT NULL,
     PRIMARY KEY (pos_id)
 );
 
@@ -77,16 +77,16 @@ DROP TABLE IF EXISTS venta CASCADE;
 CREATE TABLE venta
 (
     venta_id        VARCHAR DEFAULT nextval('venta_seq'::regclass) NOT NULL,
-    venta_cedula    VARCHAR(60) NOT NULL,
-    venta_nombre    VARCHAR(90) NOT NULL,
-    venta_direccion VARCHAR(90) NOT NULL,
-    venta_fecha     DATE        NOT NULL,
-    venta_metodo    VARCHAR(30) NOT NULL,
-    venta_seguro    DECIMAL     NOT NULL,
-    venta_subtotal  DECIMAL     NOT NULL,
-    venta_iva       DECIMAL     NOT NULL,
-    venta_total     DECIMAL     NOT NULL,
-    pos_id          VARCHAR(30) NOT NULL,
+    venta_cedula    VARCHAR NOT NULL,
+    venta_nombre    VARCHAR NOT NULL,
+    venta_direccion VARCHAR NOT NULL,
+    venta_fecha     DATE    NOT NULL,
+    venta_metodo    VARCHAR NOT NULL,
+    venta_seguro    DECIMAL NOT NULL,
+    venta_subtotal  DECIMAL NOT NULL,
+    venta_iva       DECIMAL NOT NULL,
+    venta_total     DECIMAL NOT NULL,
+    pos_id          VARCHAR NOT NULL,
     PRIMARY KEY (venta_id),
     FOREIGN KEY (pos_id) REFERENCES pos (pos_id)
 );
